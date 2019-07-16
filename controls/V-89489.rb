9a81f34a8d5473da205db4159d51cc0d58d6677c
kclinden <kaseylinden@gmail.com>
2019-07-16 08:26:09 -0500

    fixed audit.conf path

--- controls/V-89489.rb
+++ controls/V-89489.rb
@@ -68,7 +68,7 @@ if the email address of the system administrator is on a remote system,
 Restart the audit service:
 # service auditd restart"
 
-describe auditd_conf('path') do
+describe auditd_conf('/etc/audit/auditd.conf') do
   its('space_left_action') { should cmp 'syslog' }
   its('space_left_action') { should_not cmp 'ignore' }
   its('space_left_action') { should_not cmp 'suspend' }
